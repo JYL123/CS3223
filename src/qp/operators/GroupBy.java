072539bdd9360935d36c172bfbb228af1eff3e9b
junrong09 <jun_rong5@hotmail.com>
2019-04-04 21:22:03 +0800

    Add group by next method and code clean up

--- src/qp/operators/GroupBy.java
+++ src/qp/operators/GroupBy.java
@@ -7,6 +7,7 @@ package qp.operators;
 import qp.utils.*;
 
 import java.io.EOFException;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -28,16 +29,14 @@ public class GroupBy extends Operator {
     Vector attrSet;
     int batchsize;  // number of tuples per outbatch
 
-    Batch inbatch;
     Batch outbatch;
     int numBuffer;
-    HashSet<Vector> hashSet;
 
     static int filenum = -1;   // To get unique filenum for this operation
     int currentFileNum; // Hash Join instance unique file num (to be used to generate output file name)
     ArrayList<String> fileNames = new ArrayList<>(); // File names of all partitions generated during partition phase
+    int pageCurs; // To resume page reading after each next
 
-    int inputCurs;
     /**
      * index of the attributes in the base operator
      * * that are to be group by
@@ -50,7 +49,6 @@ public class GroupBy extends Operator {
         super(type);
         this.base = base;
         this.attrSet = as;
-
     }
 
     public void setBase(Operator base) {
@@ -75,9 +73,8 @@ public class GroupBy extends Operator {
         /** setnumber of tuples per batch **/
         int tuplesize = schema.getTupleSize();
         batchsize = Batch.getPageSize() / tuplesize;
-        hashSet = new HashSet<>();
         numBuffer = 3; // Min buffer required
-        inputCurs = -1;
+        pageCurs = 0;
         currentFileNum = ++filenum;
         passCounts = new ArrayList<>();
 
@@ -185,105 +182,42 @@ public class GroupBy extends Operator {
                     }
                     runCurs = i;
                     // Load all runs into b -1 buffers
-                    Iterator<Tuple> it = new TupleIterator(generateFileNamePrefix(pass, runCurs), preRunCounts.get(runCurs));
+                    Iterator<Tuple> it = new TupleIterator(generateFileNamePrefix(pass, runCurs),
+                            preRunCounts.get(runCurs));
                     its.add(it);
                 }
 
-                int pagesGenerated = merge(its, buffers[buffers.length -1 ], pass + 1, newRunCount);
+                int pagesGenerated = merge(its, buffers[buffers.length - 1], pass + 1, newRunCount);
                 curRunCount.add(newRunCount, pagesGenerated);
                 newRunCount++;
                 its = new ArrayList<>(numBuffer - 1);
                 // End of one new run
             }
-        // End of a pass
+            // End of a pass
         }
 
         return true;
     }
-
-    private int merge(ArrayList<Iterator<Tuple>> its, Batch output, int nextPass, int run) {
-        int pageNum = -1;
-        TreeMap<Vector, Integer> map = new TreeMap<>(getComparator());
-
-        // Initial loading
-        for (int i = 0; i < its.size(); i++) {
-            Iterator<Tuple> it = its.get(i);
-            while (it.hasNext()) {
-                Vector v = it.next().data();
-                if (!map.containsKey(v)) {
-                    // No duplicate, go to next iterator
-                    map.put(v, i);
-                    break;
-                }
-            }
-        }
-
-        // Polling of tuples and replacing
-        while (!map.isEmpty()) {
-            Map.Entry<Vector, Integer> outputVector = map.pollFirstEntry();
-            // Put in batch, write if full
-            output.add(new Tuple(outputVector.getKey()));
-            if (output.isFull()) {
-                pageNum++;
-                pageWrite(output, generateFileName(nextPass, run, pageNum));
-                output.clear();
-            }
-            // Replace with next tuple from that particular run
-            int index = outputVector.getValue();
-            Iterator<Tuple> it = its.get(index);
-            while (it.hasNext()) {
-                Vector v = it.next().data();
-                if (!map.containsKey(v)) {
-                    // No duplicate, go to next iterator
-                    map.put(v, index);
-                    break;
-                }
-            }
-        }
-
-        if (!output.isEmpty()) {
-            pageNum++;
-            pageWrite(output, generateFileName(nextPass, run, pageNum));
-            System.out.println(generateFileName(nextPass, run, pageNum));
-            Debug.PPrint(output);
-            output.clear();
-        }
-
-        return pageNum + 1;
-    }
-
 
     /**
      * Read next tuple from operator
      */
 
     public Batch next() {
-        //System.out.println("Project:-----------------in next-----------------");
-        outbatch = new Batch(batchsize);
+        this.outbatch = null;
 
-        // all the tuples in the inbuffer goes to the output buffer
-        inbatch = base.next();
-        // System.out.println("Project:-------------- inside the next---------------");
-
-        if (inbatch == null) {
-            return null;
+        int pass = passCounts.size() - 1;
+        int run = passCounts.get(pass).size() - 1;
+        if (run > 0) {
+            System.out.println("Runs not merged!");
         }
+        int pages = passCounts.get(pass).get(run);
 
-        //System.out.println("Project:---------------base tuples---------");
-        for (int i = inputCurs + 1; i < inbatch.size(); i++) {
-            inputCurs = i;
-            Tuple basetuple = inbatch.elementAt(i);
-            Vector present = getGroupByValues(basetuple);
-            if (!hashSet.contains(present)) {
-                hashSet.add(present);
-                Tuple outtuple = new Tuple(present);
-                outbatch.add(outtuple);
-                if (outbatch.isFull()) {
-                    return outbatch;
-                }
-            }
+        if (pageCurs < pages) {
+            // Each file read match with batch size
+            pageRead(generateFileName(pass, run, pageCurs));
+            pageCurs++;
         }
-        inputCurs = -1;
 
         return outbatch;
     }
@@ -293,13 +227,15 @@ public class GroupBy extends Operator {
      * Close the operator
      */
     public boolean close() {
-        return true;
-		/*
-	if(base.close())
-	    return true;
-	else
-	    return false;
-	    **/
+        for (int i = 0; i < fileNames.size(); i++) {
+            File f = new File(fileNames.get(i));
+            f.delete();
+        }
+
+        if (base.close())
+            return true;
+        else
+            return false;
     }
 
 
@@ -338,6 +274,35 @@ public class GroupBy extends Operator {
             System.out.println("Group By : writing the temporary file error === " + fileName);
             return false;
         }
+        return true;
+    }
+
+    private boolean pageRead(String fileName) {
+        ObjectInputStream in;
+        try {
+            in = new ObjectInputStream(new FileInputStream(fileName));
+        } catch (IOException io) {
+            System.err.println("Hash Join : error in reading the file === " + fileName);
+            return false;
+        }
+
+        try {
+            this.outbatch = (Batch) in.readObject();
+            in.close();
+        } catch (EOFException e) {
+            try {
+                in.close();
+            } catch (IOException io) {
+                System.out.println("Hash Join :Error in temporary file reading === " + fileName);
+            }
+        } catch (ClassNotFoundException c) {
+            System.out.println("Hash Join :Some error in deserialization  === " + fileName);
+            System.exit(1);
+        } catch (IOException io) {
+            System.out.println("Hash Join :temporary file reading error  === " + fileName);
+            System.exit(1);
+        }
+
         return true;
     }
 
@@ -380,5 +345,58 @@ public class GroupBy extends Operator {
             }
             return 0;
         };
+    }
+
+    private int merge(ArrayList<Iterator<Tuple>> its, Batch output, int nextPass, int run) {
+        int pageNum = -1;
+        TreeMap<Vector, Integer> map = new TreeMap<>(getComparator());
+
+        // Initial loading
+        for (int i = 0; i < its.size(); i++) {
+            Iterator<Tuple> it = its.get(i);
+            while (it.hasNext()) {
+                Vector v = it.next().data();
+                if (!map.containsKey(v)) {
+                    // No duplicate, go to next iterator
+                    map.put(v, i);
+                    break;
+                }
+            }
+        }
+
+        // Polling of tuples and replacing
+        while (!map.isEmpty()) {
+            Map.Entry<Vector, Integer> outputVector = map.pollFirstEntry();
+            // Put in batch, write if full
+            output.add(new Tuple(outputVector.getKey()));
+            if (output.isFull()) {
+                pageNum++;
+                pageWrite(output, generateFileName(nextPass, run, pageNum));
+                System.out.println(generateFileName(nextPass, run, pageNum));
+                Debug.PPrint(output);
+                output.clear();
+            }
+            // Replace with next tuple from that particular run
+            int index = outputVector.getValue();
+            Iterator<Tuple> it = its.get(index);
+            while (it.hasNext()) {
+                Vector v = it.next().data();
+                if (!map.containsKey(v)) {
+                    // No duplicate, go to next iterator
+                    map.put(v, index);
+                    break;
+                }
+            }
+        }
+
+        if (!output.isEmpty()) {
+            pageNum++;
+            pageWrite(output, generateFileName(nextPass, run, pageNum));
+            System.out.println(generateFileName(nextPass, run, pageNum));
+            Debug.PPrint(output);
+            output.clear();
+        }
+
+        return pageNum + 1;
     }
 }
