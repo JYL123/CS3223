ee8de24872a71a9d2e2fbc2faf1be6fbfca0573e
junrong09 <jun_rong5@hotmail.com>
2019-04-04 15:41:54 +0800

    Add groupby initial run function.

--- src/qp/operators/GroupBy.java
+++ src/qp/operators/GroupBy.java
@@ -6,8 +6,18 @@ package qp.operators;
 
 import qp.utils.*;
 
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Objects;
+import java.util.TreeSet;
 import java.util.Vector;
 
 public class GroupBy extends Operator {
@@ -18,7 +28,12 @@ public class GroupBy extends Operator {
 
     Batch inbatch;
     Batch outbatch;
+    int numBuffer;
     HashSet<Vector> hashSet;
+
+    static int filenum = -1;   // To get unique filenum for this operation
+    int currentFileNum; // Hash Join instance unique file num (to be used to generate output file name)
+    ArrayList<String> fileNames = new ArrayList<>(); // File names of all partitions generated during partition phase
 
     int inputCurs;
     /**
@@ -26,6 +41,7 @@ public class GroupBy extends Operator {
      * * that are to be group by
      **/
     int[] attrIndex;
+    ArrayList<ArrayList<Integer>> passCounts;
 
 
     public GroupBy(Operator base, Vector as, int type) {
@@ -58,30 +74,115 @@ public class GroupBy extends Operator {
         int tuplesize = schema.getTupleSize();
         batchsize = Batch.getPageSize() / tuplesize;
         hashSet = new HashSet<>();
+        numBuffer = 3; // Min buffer required
         inputCurs = -1;
+        currentFileNum = ++filenum;
+        passCounts = new ArrayList<>();
 
         /** The followingl loop findouts the index of the columns that
          ** are required from the base operator
          **/
-
         Schema baseSchema = base.getSchema();
         attrIndex = new int[attrSet.size()];
-        //System.out.println("Project---Schema: ----------in open-----------");
-        //System.out.println("base Schema---------------");
-        //Debug.PPrint(baseSchema);
+
         for (int i = 0; i < attrSet.size(); i++) {
             Attribute attr = (Attribute) attrSet.elementAt(i);
             int index = baseSchema.indexOf(attr);
             attrIndex[i] = index;
-
-            //  Debug.PPrint(attr);
-            //System.out.println("  "+index+"  ");
         }
 
-        if (base.open())
-            return true;
-        else
+        if (!base.open())
             return false;
+
+        /*
+         * Sorting phase - In-memory sorting
+         * Post-condition: Generate sorted initial run
+         */
+        Batch[] buffers = new Batch[numBuffer];
+
+        buffers[0] = base.next();
+        int initialRun = -1;
+
+        while (buffers[0] != null) {
+            initialRun++;
+            // Fill all buffers
+            for (int i = 1; i < buffers.length; i++) {
+                buffers[i] = base.next();
+                if (buffers[i] == null) {
+                    break;
+                }
+            }
+
+            // Extract all vectors from tuples in the batches
+            // TreeSet sort and eliminate duplicates
+            TreeSet<Vector> orderedVectors = new TreeSet<>(getComparator());
+            for (int i = 0; i < buffers.length; i++) {
+                if (buffers[i] == null) {
+                    break;
+                }
+
+                Iterator<Tuple> it = buffers[i].getIterator();
+                while (it.hasNext()) {
+                    orderedVectors.add(getGroupByValues(it.next()));
+                }
+            }
+
+            // Write initial runs
+            Iterator<Vector> it = orderedVectors.iterator();
+            buffers[0] = new Batch(batchsize);
+            int pageNum = -1;
+            while (it.hasNext()) {
+                Tuple t = new Tuple(it.next());
+                buffers[0].add(t);
+                Debug.PPrint(t);
+
+                if (buffers[0].isFull()) {
+                    pageNum++;
+                    pageWrite(buffers[0], generateFileName(0, initialRun, pageNum));
+                    buffers[0].clear();
+                }
+            }
+            if (!buffers[0].isEmpty()) {
+                pageNum++;
+                pageWrite(buffers[0], generateFileName(0, initialRun, pageNum));
+            }
+
+            buffers[0] = base.next();
+        }
+
+        /*
+         * Merge phase
+         */
+
+        return true;
+    }
+
+    private Comparator<Vector> getComparator() {
+        return (x, y) -> {
+            for (int i = 0; i < x.size(); i++) {
+                Object leftdata = x.get(i);
+                Object rightdata = y.get(i);
+                int comp = 0;
+                if (leftdata instanceof Integer) {
+                    comp = ((Integer) leftdata).compareTo((Integer) rightdata);
+                } else if (leftdata instanceof String) {
+                    comp = ((String) leftdata).compareTo((String) rightdata);
+
+                } else if (leftdata instanceof Float) {
+                    comp = ((Float) leftdata).compareTo((Float) rightdata);
+                } else {
+                    System.out.println("Tuple: Unknown comparision of the tuples");
+                    System.exit(1);
+                    return 0;
+                }
+                // Attribute is not equal (differences found)
+                // If Attribute is equal (continue to compare the next attribute)
+                if (comp != 0) {
+                    return comp;
+                }
+            }
+            return 0;
+        };
     }
 
     /**
@@ -151,5 +252,68 @@ public class GroupBy extends Operator {
             values.add(t.dataAt(i));
         }
         return values;
+    }
+
+    /**
+     * Batch write out.
+     *
+     * @param b        page to be written out
+     * @param fileName name of the file
+     */
+    private boolean pageWrite(Batch b, String fileName) {
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName));
+            out.writeObject(b);
+            fileNames.add(fileName);
+            out.close();
+        } catch (IOException io) {
+            System.out.println("Group By : writing the temporary file error === " + fileName);
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Read batch into input buffer.
+     *
+     * @param fileName name of the file to be read
+     * @return True if page has been read into input buffer
+     */
+    private boolean pageRead(Batch b, String fileName) {
+        ObjectInputStream in;
+        try {
+            in = new ObjectInputStream(new FileInputStream(fileName));
+        } catch (IOException io) {
+            System.err.println("Group By : error in reading the file === " + fileName);
+            return false;
+        }
+
+        try {
+            b = (Batch) in.readObject();
+            in.close();
+        } catch (EOFException e) {
+            try {
+                in.close();
+            } catch (IOException io) {
+                System.out.println("Group By :Error in temporary file reading === " + fileName);
+            }
+        } catch (ClassNotFoundException c) {
+            System.out.println("Group By :Some error in deserialization  === " + fileName);
+            System.exit(1);
+        } catch (IOException io) {
+            System.out.println("Group By :temporary file reading error  === " + fileName);
+            System.exit(1);
+        }
+
+        return true;
+    }
+
+    /**
+     * File name generator to standardize file name used in I/O
+     *
+     * @return file name generated
+     */
+    private String generateFileName(int passNum, int runNum, int pageNum) {
+        return String.format("GBtemp%d-pass%d-run%d.%d", currentFileNum, passNum, runNum, pageNum);
     }
 }
