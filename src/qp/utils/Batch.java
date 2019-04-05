0bf86b2417d2865411c8ce2cf589bb1ecf98dbb5
Shuoyang Zhang <>
2019-04-05 15:47:23 +0800

    solving merge conflict

--- src/qp/utils/Batch.java
+++ src/qp/utils/Batch.java
@@ -23,43 +23,23 @@ public class Batch implements Serializable {
     /** get number of bytes per page **/
     public static int getPageSize() {
         return PageSize;
-=======
-	/** Set number of bytes per page **/
-	public static void setPageSize(int size){
-		PageSize=size;
-	}
-
-	/** get number of bytes per page **/
-	public static int getPageSize(){
-		return PageSize;
-	}
+    }
 
 	/** Number of tuples per page **/
-
     public Batch(int numtuple){
 		MAX_SIZE = numtuple;
 	    tuples = new Vector(MAX_SIZE);
     }
 
-    /** Number of tuples per page **/
-    public Batch(int numtuple) {
-        MAX_SIZE = numtuple;
-        tuples = new Vector(MAX_SIZE);
-    }
 
     /** insert the record in page at next free location**/
     public void add(Tuple t) {
         tuples.add(t);
     }
 
-<<<<<<< HEAD
-    public int capacity() {
-        return MAX_SIZE;
-=======
+
     public int capacity(){
 	    return MAX_SIZE;
-
->>>>>>> origin/NEW_DISTINCT
     }
 
     public void clear() {
@@ -86,17 +66,13 @@ public class Batch implements Serializable {
         return tuples.isEmpty();
     }
 
-    public void remove(int i) {
-        tuples.remove(i);
-    }
 
-<<<<<<< HEAD
     public void setElementAt(Tuple t, int i) {
         tuples.setElementAt(t, i);
-=======
+    }
+
     public Tuple remove(int i){
-	return (Tuple) tuples.remove(i);
->>>>>>> origin/NEW_DISTINCT
+	    return (Tuple) tuples.remove(i);
     }
 
     public int size() {
@@ -110,15 +86,8 @@ public class Batch implements Serializable {
             return false;
     }
 
-<<<<<<< HEAD
     public Iterator<Tuple> getIterator() {
         return tuples.iterator();
-=======
-    public boolean isFull(){
-	    if(size() == capacity())
-	        return true;
-	    else
-	        return false;
->>>>>>> origin/NEW_DISTINCT
     }
+
 }
