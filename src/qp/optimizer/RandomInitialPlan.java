3150960936b0bf438ad08b0911bd2f0d9bfa7eea
Shuoyang Zhang <>
2019-04-05 16:08:59 +0800

    solving merge conflict

--- src/qp/optimizer/RandomInitialPlan.java
+++ src/qp/optimizer/RandomInitialPlan.java
@@ -1,6 +1,10 @@
 /**
  * prepares a random initial plan for the given SQL query  see the ReadMe file to understand this
  * see the ReadMe file to understand this
+ * see the ReadMe file to understand this
+ * see the ReadMe file to understand this
+ * see the ReadMe file to understand this
+ * see the ReadMe file to understand this
  **/
 /** see the ReadMe file to understand this **/
 
@@ -69,7 +73,6 @@ public class RandomInitialPlan {
             createJoinOp();
         }
         createDistinctOp();
-
         createProjectOp();
         // TODO: GROUPBY DONE
         createGroupbyOp();
@@ -121,35 +124,9 @@ public class RandomInitialPlan {
             root = tempop;
             return;
         }
-        /** Read the schema of the table from tablename.md file
-         ** md stands for metadata
-         **/
 
-        String filename = tabname + ".md";
-        try {
-            ObjectInputStream _if = new ObjectInputStream(new FileInputStream(filename));
-            Schema schm = (Schema) _if.readObject();
-            op1.setSchema(schm);
-            _if.close();
-        } catch (Exception e) {
-            System.err.println("RandomInitialPlan:Error reading Schema of the table" + filename);
-            System.exit(1);
-        }
-        tab_op_hash.put(tabname, op1);
+
     }
-
-    // 12 July 2003 (whtok)
-    // To handle the case where there is no where clause
-    // selectionlist is empty, hence we set the root to be
-    // the scan operator. the projectOp would be put on top of
-    // this later in CreateProjectOp
-        if(selectionlist.size()==0)
-
-    {
-        root = tempop;
-        return;
-    }
-
 
 
     /** Create Selection Operators for each of the
@@ -174,187 +151,121 @@ public class RandomInitialPlan {
 
                 modifyHashtable(tempop, op1);
                 //tab_op_hash.put(tabname,op1);
-
-                /** create join operators **/
-
-                public void createJoinOp () {
-                    BitSet bitCList = new BitSet(numJoin);
-                    int jnnum = RandNumb.randInt(0, numJoin - 1);
-                    Join jn = null;
-                    /** Repeat until all the join conditions are considered **/
-                    while (bitCList.cardinality() != numJoin) {
-                        /** If this condition is already consider chose
-                         ** another join condition
-                         **/
-
-                        while (bitCList.get(jnnum)) {
-                            jnnum = RandNumb.randInt(0, numJoin - 1);
-                        }
-                        Condition cn = (Condition) joinlist.elementAt(jnnum);
-                        String lefttab = cn.getLhs().getTabName();
-                        String righttab = ((Attribute) cn.getRhs()).getTabName();
-
-                        // System.out.println("---------JOIN:---------left X right"+lefttab+righttab);
-
-                        Operator left = (Operator) tab_op_hash.get(lefttab);
-                        Operator right = (Operator) tab_op_hash.get(righttab);
-                        jn = new Join(left, right, cn, OpType.JOIN);
-                        jn.setNodeIndex(jnnum);
-                        Schema newsche = left.getSchema().joinWith(right.getSchema());
-                        jn.setSchema(newsche);
-                        /** randomly select a join type**/
-                        int numJMeth = JoinType.numJoinTypes();
-                        int joinMeth = RandNumb.randInt(0, numJMeth - 1);
-                        jn.setJoinType(joinMeth);
-
-                        modifyHashtable(left, jn);
-                        modifyHashtable(right, jn);
-                        //tab_op_hash.put(lefttab,jn);
-                        //tab_op_hash.put(righttab,jn);
-
-                        bitCList.set(jnnum);
-                    }
-                    /** The last join operation is the root for the
-                     ** constructed till now
-                     **/
-
-                    if (numJoin != 0)
-                        root = jn;
-=======
-                }
             }
-            /** The last selection is the root of the plan tre
-             ** constructed thus far
-             **/
-            if (selectionlist.size() != 0)
-                root = op1;
->>>>>>>origin / NEW_DISTINCT
         }
+        /** The last selection is the root of the plan tre
+         ** constructed thus far
+         **/
+        if (selectionlist.size() != 0)
+            root = op1;
+    }
 
-        /** create join operators **/
 
-        public void createJoinOp () {
-            BitSet bitCList = new BitSet(numJoin);
-            int jnnum = RandNumb.randInt(0, numJoin - 1);
-            Join jn = null;
-            /** Repeat until all the join conditions are considered **/
-            while (bitCList.cardinality() != numJoin) {
-                /** If this condition is already consider chose
-                 ** another join condition
-                 **/
+    /** create join operators **/
 
-                while (bitCList.get(jnnum)) {
-                    jnnum = RandNumb.randInt(0, numJoin - 1);
-                }
-                Condition cn = (Condition) joinlist.elementAt(jnnum);
-                String lefttab = cn.getLhs().getTabName();
-                String righttab = ((Attribute) cn.getRhs()).getTabName();
-
-                // System.out.println("---------JOIN:---------left X right"+lefttab+righttab);
-
-                Operator left = (Operator) tab_op_hash.get(lefttab);
-                Operator right = (Operator) tab_op_hash.get(righttab);
-                jn = new Join(left, right, cn, OpType.JOIN);
-                jn.setNodeIndex(jnnum);
-                Schema newsche = left.getSchema().joinWith(right.getSchema());
-                jn.setSchema(newsche);
-                /** randomly select a join type**/
-                int numJMeth = JoinType.numJoinTypes();
-                int joinMeth = RandNumb.randInt(0, numJMeth - 1);
-                jn.setJoinType(joinMeth);
-
-                modifyHashtable(left, jn);
-                modifyHashtable(right, jn);
-                //tab_op_hash.put(lefttab,jn);
-                //tab_op_hash.put(righttab,jn);
-
-                bitCList.set(jnnum);
-            }
-            /** The last join operation is the root for the
-             ** constructed till now
+    public void createJoinOp() {
+        BitSet bitCList = new BitSet(numJoin);
+        int jnnum = RandNumb.randInt(0, numJoin - 1);
+        Join jn = null;
+        /** Repeat until all the join conditions are considered **/
+        while (bitCList.cardinality() != numJoin) {
+            /** If this condition is already consider chose
+             ** another join condition
              **/
 
-            if (numJoin != 0)
-                root = jn;
+            while (bitCList.get(jnnum)) {
+                jnnum = RandNumb.randInt(0, numJoin - 1);
+            }
+            Condition cn = (Condition) joinlist.elementAt(jnnum);
+            String lefttab = cn.getLhs().getTabName();
+            String righttab = ((Attribute) cn.getRhs()).getTabName();
+
+            // System.out.println("---------JOIN:---------left X right"+lefttab+righttab);
+
+            Operator left = (Operator) tab_op_hash.get(lefttab);
+            Operator right = (Operator) tab_op_hash.get(righttab);
+            jn = new Join(left, right, cn, OpType.JOIN);
+            jn.setNodeIndex(jnnum);
+            Schema newsche = left.getSchema().joinWith(right.getSchema());
+            jn.setSchema(newsche);
+            /** randomly select a join type**/
+            int numJMeth = JoinType.numJoinTypes();
+            int joinMeth = RandNumb.randInt(0, numJMeth - 1);
+            jn.setJoinType(joinMeth);
+
+            modifyHashtable(left, jn);
+            modifyHashtable(right, jn);
+            //tab_op_hash.put(lefttab,jn);
+            //tab_op_hash.put(righttab,jn);
+
+            bitCList.set(jnnum);
+        }
+        /** The last join operation is the root for the
+         ** constructed till now
+         **/
+
+        if (numJoin != 0)
+            root = jn;
+    }
+
+
+    public void createProjectOp() {
+        Operator base = root;
+        if (projectlist == null)
+            projectlist = new Vector();
+
+
+        if (!projectlist.isEmpty()) {
+            root = new Project(base, projectlist, OpType.PROJECT);
+            Schema newSchema = base.getSchema().subSchema(projectlist);
+            root.setSchema(newSchema);
+        }
+    }
+
+    public void createGroupbyOp() {
+        // TODO: GROUPBY DONE
+        Operator base = root;
+        if (groupbylist == null)
+            groupbylist = new Vector();
+
+        if (!groupbylist.isEmpty()) {
+            root = new GroupBy(base, groupbylist, OpType.GROUPBY);
+            Schema newSchema = base.getSchema().subSchema(groupbylist);
+            root.setSchema(newSchema);
+        }
+    }
+
+
+    private void createDistinctOp() {
+        Operator base = root;
+
+        if (isDistinct) {
+            Schema newSchema;
+            if (projectlist.isEmpty()) {
+                projectlist = base.getSchema().getAttList();
+                newSchema = base.getSchema();
+            } else {
+                newSchema = base.getSchema().subSchema(projectlist);
+            }
+            root = new Distinct(base, projectlist, OpType.DISTINCT);
+            root.setSchema(newSchema);
+        }
+    }
+
+    private void modifyHashtable(Operator old, Operator newop) {
+        Enumeration e = tab_op_hash.keys();
+        while (e.hasMoreElements()) {
+            String key = (String) e.nextElement();
+            Operator temp = (Operator) tab_op_hash.get(key);
+            if (temp == old) {
+                tab_op_hash.put(key, newop);
+            }
         }
 
-
-<<<<<<<HEAD
-        public void createProjectOp () {
-            Operator base = root;
-            System.out.println("root is: ");
-            System.out.println(root);
-            if (projectlist == null)
-                projectlist = new Vector();
-=======
-            public void createProjectOp () {
-                Operator base = root;
-                if (projectlist == null)
-                    projectlist = new Vector();
->>>>>>>origin / NEW_DISTINCT
+    }
 
 
-                if (!projectlist.isEmpty()) {
-                    root = new Project(base, projectlist, OpType.PROJECT);
-                    Schema newSchema = base.getSchema().subSchema(projectlist);
-                    root.setSchema(newSchema);
-                }
-            }
-
-<<<<<<<HEAD
-            public void createGroupbyOp () {
-                // TODO: GROUPBY DONE
-                Operator base = root;
-                if (groupbylist == null)
-                    groupbylist = new Vector();
-
-                if (!groupbylist.isEmpty()) {
-                    root = new GroupBy(base, groupbylist, OpType.GROUPBY);
-                    Schema newSchema = base.getSchema().subSchema(groupbylist);
-                    root.setSchema(newSchema);
-                }
-            }
-
-            private void modifyHashtable (Operator old, Operator newop){
-                Enumeration e = tab_op_hash.keys();
-                while (e.hasMoreElements()) {
-                    String key = (String) e.nextElement();
-                    Operator temp = (Operator) tab_op_hash.get(key);
-                    if (temp == old) {
-                        tab_op_hash.put(key, newop);
-                    }
-                }
-=======
-                private void createDistinctOp () {
-                    Operator base = root;
-
-                    if (isDistinct) {
-                        Schema newSchema;
-                        if (projectlist.isEmpty()) {
-                            projectlist = base.getSchema().getAttList();
-                            newSchema = base.getSchema();
-                        } else {
-                            newSchema = base.getSchema().subSchema(projectlist);
-                        }
-                        root = new Distinct(base, projectlist, OpType.DISTINCT);
-                        root.setSchema(newSchema);
-                    }
->>>>>>>origin / NEW_DISTINCT
-                }
-
-                private void modifyHashtable (Operator old, Operator newop){
-                    Enumeration e = tab_op_hash.keys();
-                    while (e.hasMoreElements()) {
-                        String key = (String) e.nextElement();
-                        Operator temp = (Operator) tab_op_hash.get(key);
-                        if (temp == old) {
-                            tab_op_hash.put(key, newop);
-                        }
-                    }
-                }
-
-
-            }
+}
 
 
 
